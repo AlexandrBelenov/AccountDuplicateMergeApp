f1b221c5be8735c45a01947c2e29b79e37c87a9a
AlexandrBelenov <belenoff.s@gmail.com>
2022-07-11 01:48:45 +0300

    added batchable jobs

--- force-app/main/default/lwc/duplicateAccountReport/duplicateAccountReport.js
+++ force-app/main/default/lwc/duplicateAccountReport/duplicateAccountReport.js
@@ -5,20 +5,39 @@
 import { LightningElement } from "lwc";
 import runReport from '@salesforce/apex/ReportController.runReport';
 import getDuplicateRecordSets from '@salesforce/apex/ReportController.getDuplicateRecordSets';
+import runReportBatch from '@salesforce/apex/ReportController.runReportBatch'
 
 export default class DuplicateAccountReport extends LightningElement {
   duplicateRecordSetList;
+  batchId;
 
-  handleClick(e) {
-      runReport().then(result => {
-        this.duplicateRecordSetList = result;
+  handleRunReportBatch(e) {
+      runReportBatch().then(result => {
+        this.batchId = result;
       }).catch(error => {
         console.log(error)
-      })
+      });
+      console.log(this.batchId);
+  }
+
+  handleRefreshDuplicates(e) {
+    getDuplicateRecordSets().then(result => {
+      this.duplicateRecordSetList = result;
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+  handleRunReport(e) {
+    runReport().then(result => {
+      this.duplicateRecordSetList = result;
+    }).catch(error => {
+      console.log(error)
+    })
   }
 
   handleModalClosed(e) {
-    getDuplicateRecordSets.then(result => {
+    getDuplicateRecordSets().then(result => {
       this.duplicateRecordSetList = result;
     }).catch(error => {
       console.log(error)
